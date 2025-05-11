8ad5a4d79031987691ee82de1f692b3775b90011
Rahul Menon <menonrahul02@gmail.com>
2025-05-11 12:52:16 -0500

    working faster version using reversing

--- p1/everybit/bitarray.c
+++ p1/everybit/bitarray.c
@@ -24,15 +24,12 @@
 // array containing bit_sz bits will consume roughly bit_sz/8 bytes of
 // memory.
 
-
 #include "./bitarray.h"
 
 #include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
 #include <sys/types.h>
-
 
 // ********************************* Types **********************************
 
@@ -46,7 +43,6 @@ struct bitarray {
   // packed form (8 per byte).
   char* buf;
 };
-
 
 // ******************** Prototypes for static functions *********************
 
@@ -107,12 +103,14 @@ static size_t modulo(const ssize_t n, const size_t m);
 // not matter.
 static char bitmask(const size_t bit_index);
 
+static void bitarray_reverse(bitarray_t* const bitarray,
+                             const size_t bit_offset, const size_t bit_length);
 
 // ******************************* Functions ********************************
 
 bitarray_t* bitarray_new(const size_t bit_sz) {
   // Allocate an underlying buffer of ceil(bit_sz/8) bytes.
-  char* const buf = calloc(1, (bit_sz+7) / 8);
+  char* const buf = calloc(1, (bit_sz + 7) / 8);
   if (buf == NULL) {
     return NULL;
   }
@@ -153,12 +151,10 @@ bool bitarray_get(const bitarray_t* const bitarray, const size_t bit_index) {
   // get the byte; we then bitwise-and the byte with an appropriate mask
   // to produce either a zero byte (if the bit was 0) or a nonzero byte
   // (if it wasn't).  Finally, we convert that to a boolean.
-  return (bitarray->buf[bit_index / 8] & bitmask(bit_index)) ?
-         true : false;
+  return (bitarray->buf[bit_index / 8] & bitmask(bit_index)) ? true : false;
 }
 
-void bitarray_set(bitarray_t* const bitarray,
-                  const size_t bit_index,
+void bitarray_set(bitarray_t* const bitarray, const size_t bit_index,
                   const bool value) {
   assert(bit_index < bitarray->bit_sz);
 
@@ -170,21 +166,19 @@ void bitarray_set(bitarray_t* const bitarray,
   // to clear out the bit we're about to set.  We bitwise-or the result
   // with a byte that has either a 1 or a 0 in the correct place.
   bitarray->buf[bit_index / 8] =
-    (bitarray->buf[bit_index / 8] & ~bitmask(bit_index)) |
-    (value ? bitmask(bit_index) : 0);
+      (bitarray->buf[bit_index / 8] & ~bitmask(bit_index)) |
+      (value ? bitmask(bit_index) : 0);
 }
 
-void bitarray_randfill(bitarray_t* const bitarray){
-  int32_t *ptr = (int32_t *)bitarray->buf;
-  for (int64_t i=0; i<bitarray->bit_sz/32 + 1; i++){
+void bitarray_randfill(bitarray_t* const bitarray) {
+  int32_t* ptr = (int32_t*)bitarray->buf;
+  for (int64_t i = 0; i < bitarray->bit_sz / 32 + 1; i++) {
     ptr[i] = rand();
   }
 }
 
-void bitarray_rotate(bitarray_t* const bitarray,
-                     const size_t bit_offset,
-                     const size_t bit_length,
-                     const ssize_t bit_right_amount) {
+void bitarray_rotate(bitarray_t* const bitarray, const size_t bit_offset,
+                     const size_t bit_length, const ssize_t bit_right_amount) {
   assert(bit_offset + bit_length <= bitarray->bit_sz);
 
   if (bit_length == 0) {
@@ -201,9 +195,10 @@ static void bitarray_rotate_left(bitarray_t* const bitarray,
                                  const size_t bit_offset,
                                  const size_t bit_length,
                                  const size_t bit_left_amount) {
-  for (size_t i = 0; i < bit_left_amount; i++) {
-    bitarray_rotate_left_one(bitarray, bit_offset, bit_length);
-  }
+  bitarray_reverse(bitarray, bit_offset, bit_left_amount);
+  bitarray_reverse(bitarray, bit_offset + bit_left_amount,
+                   bit_length - bit_left_amount);
+  bitarray_reverse(bitarray, bit_offset, bit_length);
 }
 
 static void bitarray_rotate_left_one(bitarray_t* const bitarray,
@@ -227,7 +222,16 @@ static size_t modulo(const ssize_t n, const size_t m) {
   return (size_t)result;
 }
 
-static char bitmask(const size_t bit_index) {
-  return 1 << (bit_index % 8);
-}
+static char bitmask(const size_t bit_index) { return 1 << (bit_index % 8); }
 
+static void bitarray_reverse(bitarray_t* const bitarray,
+                             const size_t bit_offset, const size_t bit_length) {
+  bool tmp;
+  for (size_t i = bit_offset; i < bit_offset + bit_length / 2; i++) {
+    // swap item at i with item at len - i
+    size_t opp_i = bit_offset + bit_length - (i - bit_offset) - 1;
+    tmp = bitarray_get(bitarray, i);
+    bitarray_set(bitarray, i, bitarray_get(bitarray, opp_i));
+    bitarray_set(bitarray, opp_i, tmp);
+  }
+}
